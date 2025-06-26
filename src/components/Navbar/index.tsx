f246f87bf80bf3782cfd08f139c8e82a6d9c8193
Rama Diaz <ramadiaz221@gmail.com>
2025-06-26 21:02:51 +0700

    feat: add nav menu with shadcn comp

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -15,45 +15,49 @@ import {
   TranslateIcon,
 } from "@phosphor-icons/react";
 import React from "react";
+import { NavigationMenuDemo } from "./navMenu";
 
 const Navbar = () => {
   return (
-    <div className="w-full px-2 py-6 border-b border-neutral-300">
-      <div className="max-w-5xl mx-auto flex flex-row gap-8 justify-between items-center">
-        <div className="flex flex-row gap-4 items-center justify-between">
-          <Image alt="logo" sizes="50px" src={"https://placehold.co/50"} />
-          <Image alt="logo" sizes="50px" src={"https://placehold.co/50"} />
-          <Image alt="logo" sizes="50px" src={"https://placehold.co/50"} />
+    <div className="w-full">
+      <div className="px-2 py-6 border-b border-neutral-300">
+        <div className="max-w-6xl mx-auto flex flex-row gap-8 justify-between items-center">
+          <div className="flex flex-row gap-4 items-center justify-between">
+            <Image alt="logo" sizes="50px" src={"https://placehold.co/50"} />
+            <Image alt="logo" sizes="50px" src={"https://placehold.co/50"} />
+            <Image alt="logo" sizes="50px" src={"https://placehold.co/50"} />
+          </div>
+          <div className="flex-grow">
+            <Input
+              placeholder="Cari Berita dan Layanan Kutamukti Disini"
+              type="search"
+              variant="bordered"
+              radius="md"
+              startContent={<MagnifyingGlassIcon size={20} weight="thin" />}
+            />
+          </div>
+          <div>
+            <Dropdown placement="bottom-end">
+              <DropdownTrigger>
+                <Button
+                  variant="light"
+                  startContent={<TranslateIcon size={20} weight="thin" />}
+                  endContent={<CaretDownIcon size={16} color="#008FD7" />}
+                >
+                  ID
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu aria-label="Static Actions">
+                <DropdownItem key="id">Bahasa Indonesia</DropdownItem>
+                <DropdownItem key="comming soon" isDisabled>Coming Soon</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
         </div>
-        <div className="flex-grow">
-          <Input
-            placeholder="Cari Berita dan Layanan Kutamukti Disini"
-            type="search"
-            variant="bordered"
-            radius="md"
-            startContent={<MagnifyingGlassIcon size={20} weight="thin" />}
-          />
-        </div>
-        <div>
-          <Dropdown>
-            <DropdownTrigger>
-              <Button
-                variant="light"
-                startContent={<TranslateIcon size={20} weight="thin" />}
-                endContent={<CaretDownIcon size={16} color="#008FD7" />}
-              >
-                ID
-              </Button>
-            </DropdownTrigger>
-            <DropdownMenu aria-label="Static Actions">
-              <DropdownItem key="new">New file</DropdownItem>
-              <DropdownItem key="copy">Copy link</DropdownItem>
-              <DropdownItem key="edit">Edit file</DropdownItem>
-              <DropdownItem key="delete" className="text-danger" color="danger">
-                Delete file
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
+      </div>
+      <div className="w-full py-4 border-b border-neutral-300">
+        <div className="max-w-6xl w-max mx-auto ">
+          <NavigationMenuDemo />
         </div>
       </div>
     </div>
---
+++ src/components/Navbar/navMenu.tsx
@@ -0,0 +1,176 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { HouseIcon } from "@phosphor-icons/react";
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Alert Dialog",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+  },
+  {
+    title: "Hover Card",
+    href: "/docs/primitives/hover-card",
+    description:
+      "For sighted users to preview content available behind a link.",
+  },
+];
+
+const profileDesa: { title: string; href: string; description: string }[] = [
+  {
+    title: "Tentang Kami",
+    href: "/profile/about",
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+  },
+  {
+    title: "Visi dan Misi",
+    href: "/profile/vision",
+    description:
+      "For sighted users to preview content available behind a link.",
+  },
+];
+
+export function NavigationMenuDemo() {
+  return (
+    <NavigationMenu className="" viewport={false}>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+            <Link href="/">
+              <HouseIcon size={32} color="#008FD7" />
+            </Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="font-normal uppercase">
+            Profil Desa
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+              {profileDesa.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="font-normal uppercase">
+            Data Desa
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="font-normal uppercase">
+            Status Desa
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="font-normal uppercase">
+            Informasi Publik
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="font-normal uppercase">
+            Produk Desa
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink className="font-normal uppercase">
+            Hubungi Kami
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+function ListItem({
+  title,
+  children,
+  href,
+  ...props
+}: React.ComponentPropsWithoutRef<"li"> & { href: string }) {
+  return (
+    <li {...props}>
+      <NavigationMenuLink asChild>
+        <Link href={href}>
+          <div className="text-sm leading-none font-medium">{title}</div>
+          <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
+            {children}
+          </p>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  );
+}
