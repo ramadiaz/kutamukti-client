ab0f561cc17eff7239fe7b4546f15edbc94246e0
Rama Diaz <ramadiaz221@gmail.com>
2025-07-10 15:27:04 +0700

    refactor: update navbar logo href

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -16,6 +16,7 @@ import {
 } from "@phosphor-icons/react";
 import React from "react";
 import { NavigationMenuDemo } from "./navMenu";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -23,9 +24,11 @@ const Navbar = () => {
       <div className="px-2 py-6 border-b border-neutral-300">
         <div className="max-w-6xl mx-auto flex flex-row gap-8 justify-between items-center">
           <div className="flex flex-row gap-4 items-center justify-between">
-            <Image alt="logo" sizes="50px" src={"https://placehold.co/50"} />
-            <Image alt="logo" sizes="50px" src={"https://placehold.co/50"} />
-            <Image alt="logo" sizes="50px" src={"https://placehold.co/50"} />
+            <Image alt="logo" width={50} height={50} src={"https://placehold.co/50"} />
+            <Image alt="logo" width={50} height={50} src={"https://placehold.co/50"} />
+            <Link href={`/created-by`}>
+              <Image alt="logo" width={50} height={50} src={"/logos/kkn-unsika.png"} />
+            </Link>
           </div>
           <div className="flex-grow">
             <Input
