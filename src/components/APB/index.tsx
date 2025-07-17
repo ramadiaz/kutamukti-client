c2a0c86677522477290956244b3e8d278f6d1bde
Rama Diaz <ramadiaz221@gmail.com>
2025-07-17 19:46:36 +0700

    refactor: update apb and population data

--- src/components/APB/index.tsx
+++ src/components/APB/index.tsx
@@ -1,45 +1,44 @@
-'use client'
+"use client";
 
-import { Image } from "@heroui/react"
+import { Image } from "@heroui/react";
 
 const APB = () => {
-    return (
-        <div className="max-w-6xl px-2 mx-auto">
-            <div className="w-full flex flex-row gap-6">
-                <div className='basis-1/3 flex justify-end'>
-                    <Image
-                        src={`/images/apb-1.png`}
-                        alt='apb visualization'
-                        height={250}
-                        width={250}
-                        className='object-cover aspect-square'
-                    />
-                </div>
-                <div className="basis-2/3">
-                    <h1 className='font-semibold text-xl mb-6'>APB Desa Tahun 2025</h1>
-                    <p>
-                        Indeks Desa Membangun (IDM) merupakan indeks komposit yang dibentuk dari tiga indeks, yaitu Indeks Ketahanan Sosial, Indeks Ketahanan Ekonomi, dan Indeks Ketahanan Ekologi/Lingkungan.
-                    </p> 
-                    <div className="bg-gradient-to-br from-neutral-200 p-4 rounded-xl space-y-4 mt-4">
-                        <p>
-                            Pendapatan Desa
-                        </p>
-                        <p className="text-center text-2xl font-semibold mb-4">
-                            Rp 4.980.709.678.987,00
-                        </p>
-                    </div>
-                    <div className="bg-gradient-to-br from-neutral-200 p-4 rounded-xl space-y-4 mt-4">
-                        <p>
-                            Belanja Desa
-                        </p>
-                        <p className="text-center text-2xl font-semibold mb-4">
-                            Rp 4.980.709.678.987,00
-                        </p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="max-w-6xl px-2 mx-auto">
+      <div className="w-full flex flex-row gap-6">
+        <div className="basis-1/3 flex justify-end">
+          <Image
+            src={`/images/apb-1.png`}
+            alt="apb visualization"
+            height={250}
+            width={250}
+            className="object-cover aspect-square"
+          />
         </div>
-    )
-}
+        <div className="basis-2/3">
+          <h1 className="font-semibold text-xl mb-6">APB Desa Tahun 2024</h1>
+          <p>
+            Anggaran Pendapatan dan Belanja Desa (APBDes) Desa Kutamukti Tahun
+            2024 adalah dokumen perencanaan keuangan yang disusun untuk mendanai
+            berbagai program dan kegiatan pembangunan di desa sepanjang tahun
+            2024.
+          </p>
+          <div className="bg-gradient-to-br from-neutral-200 p-4 rounded-xl space-y-4 mt-4">
+            <p>Pendapatan Desa</p>
+            <p className="text-center text-2xl font-semibold mb-4">
+              Rp 2.001.963.800,00
+            </p>
+          </div>
+          <div className="bg-gradient-to-br from-neutral-200 p-4 rounded-xl space-y-4 mt-4">
+            <p>Belanja Desa</p>
+            <p className="text-center text-2xl font-semibold mb-4">
+              Rp 2.002.313.352,00
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default APB
+export default APB;
--- src/components/Population/index.tsx
+++ src/components/Population/index.tsx
@@ -1,60 +1,63 @@
-'use client'
+"use client";
 
-import { Image } from '@heroui/react'
-import React from 'react'
+import { Image } from "@heroui/react";
+import React from "react";
 
 const Population = () => {
-    return (
-        <div className='max-w-6xl px-2 mx-auto py-16'>
-            <div className='w-full flex flex-row gap-4 items-start'>
-                <div className='basis-1/2 space-y-4'>
-                    <h1 className='text-xl text-emerald-700'>
-                        Demografi Penduduk
-                    </h1>
-                    <p>
-                        Memberikan informasi lengkap mengenai karakteristik demografi penduduk suatu wilayah. Mulai dari jumlah penduduk, usia, jenis kelamin, tingkat pendidikan, pekerjaan, agama, dan aspek penting lainnya yang menggambarkan komposisi populasi secara rinci.
-                    </p>
-                </div>
-                <div className='basis-1/2'>
-                    <Image
-                        src={`/images/population-1.png`}
-                        alt='population visualization'
-                        height={180}
-                        width={180}
-                        className='object-cover aspect-square'
-                    />
-                </div>
-            </div>
-            <div className='mt-8 space-y-4'>
-                <h1 className='font-semibold text-xl mb-6'>Jumlah Penduduk dan Kepala Keluarga</h1>
-                <div className='w-full flex flex-row gap-4 items-start'>
-                    <StatsBar />
-                    <StatsBar />
-                </div>
-                <div className='w-full flex flex-row gap-4 items-start'>
-                    <StatsBar />
-                    <StatsBar />
-                </div>
-                <div className='w-full flex flex-row gap-4 items-start'>
-                    <StatsBar />
-                    <StatsBar />
-                </div>
-            </div>
+  return (
+    <div className="max-w-6xl px-2 mx-auto py-16">
+      <div className="w-full flex flex-row gap-4 items-start">
+        <div className="basis-1/2 space-y-4">
+          <h1 className="text-xl text-emerald-700">Demografi Penduduk</h1>
+          <p>
+            Memberikan informasi lengkap mengenai karakteristik demografi
+            penduduk suatu wilayah. Mulai dari jumlah penduduk, usia, jenis
+            kelamin, tingkat pendidikan, pekerjaan, agama, dan aspek penting
+            lainnya yang menggambarkan komposisi populasi secara rinci.
+          </p>
         </div>
-    )
-}
-
-const StatsBar = () => {
-    return (
-        <div className='flex flex-row w-full basis-1/2 rounded-xl overflow-hidden'>
-            <div className='basis-1/4 p-4 bg-gradient-to-br from-emerald-700 to-emerald-500 text-white font-semibold text-center text-xl'>
-                {1.149}
-            </div>
-            <div className='basis-3/4 p-4 bg-gradient-to-br from-neutral-200 text-black font-semibold text-center text-xl'>
-                Penduduk
-            </div>
+        <div className="basis-1/2">
+          <Image
+            src={`/images/population-1.png`}
+            alt="population visualization"
+            height={180}
+            width={180}
+            className="object-cover aspect-square"
+          />
         </div>
-    )
-}
+      </div>
+      <div className="mt-8 space-y-4">
+        <h1 className="font-semibold text-xl mb-6">
+          Jumlah Penduduk dan Kepala Keluarga
+        </h1>
+        <div className="w-full flex flex-row gap-4 items-start">
+          <StatsBar name={`Penduduk`} value={4433} />
+          <StatsBar name={`Laki-Laki`} value={2257} />
+        </div>
+        <div className="w-full flex flex-row gap-4 items-start">
+          <StatsBar name={`Rukun Warga`} value={5} />
+          <StatsBar name={`Perempuan`} value={2176} />
+        </div>
+        <div className="w-full flex flex-row gap-4 items-start">
+          <StatsBar name={`Rukun Tetangga`} value={18} />
+          <StatsBar name={`Kepadatan Penduduk`} value={1862} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Population
+const StatsBar = ({ name, value }: { name: string; value: number }) => {
+  return (
+    <div className="flex flex-row w-full basis-1/2 rounded-xl overflow-hidden">
+      <div className="basis-1/4 p-4 bg-gradient-to-br from-emerald-700 to-emerald-500 text-white font-semibold text-center text-xl">
+        {value.toLocaleString(`id-ID`)}
+      </div>
+      <div className="basis-3/4 p-4 bg-gradient-to-br from-neutral-200 text-black font-semibold text-center text-xl">
+        {name}
+      </div>
+    </div>
+  );
+};
+
+export default Population;
